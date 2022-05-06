05210f796c136cd170fb2bf9ca98168db9ef3f5d
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-05-06 15:08:25 -0300

    feat(SubmitFeedbackService): add image to email body

--- src/services/SubmitFeedbackService.ts
+++ src/services/SubmitFeedbackService.ts
@@ -33,8 +33,11 @@ export class SubmitFeedbackService {
       subject: 'New Feedback',
       body: [
         '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
-        `<p>Feedback type: ${type}`,
-        `<p>Comment: ${comment}`,
+        `<p>Feedback type: ${type}</p>`,
+        `<p>Comment: ${comment}</p>`,
+        screenshot
+          ? `<img src="${screenshot}" alt="Feedback screenshot" />`
+          : ``,
         '</div>'
       ].join('\n')
     });
